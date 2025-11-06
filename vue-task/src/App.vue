e85d94ae7ce655c1b8f5b580b08026a198db57d8
gaoliuqing9178 <lx8nb12act@163.com>
2025-11-06 20:46:16 +0800

    canton

--- vue-task/src/App.vue
+++ vue-task/src/App.vue
@@ -1,39 +1,30 @@
+<template>
+  <audio v-if="auto" autoplay="autoplay" src="../music/DJ阿轩 - 半点心.mp3" ></audio>
+  <input type="text" placeholder="Where is this?"></input>
+<button @click="play">我确定</button>
+  <h1>{{ uinput }}</h1>
+</template>
+
 <script setup>
 import { ref } from 'vue'
-const name = ref('555664')
-const unumber = ref(777899)
-let data = ref(null)
-function addunumber() {
-  unumber.value++
-}
-async function getData() {
-  try {
-    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
-    if (!res.ok) {
-      throw new Error(`请求失败: ${res.status}`)
-    }
-    const udata = await res.json()
-    data.value = udata.title
-    console.log(data.value)
-  } catch (error) {
-    console.error(error)
+import { reactive } from 'vue'
+let auto = ref(false)
+
+function play() {
+  const uinput = document.querySelector('input').value
+  if (uinput === `canton` || uinput === `Canton` || uinput === `CANTON`) {
+  auto.value = !auto.value
   }
 }
 </script>
 
-<template>
-  <input type="text" v-model="name" />
-  <h1>hello world</h1>
-  <h1>{{ name }}</h1>
-  <h1>{{ unumber }}</h1>
-  <button @click="addunumber">+1</button>
-  <button @click="getData">getData</button>
-  <h1>{{ data }}</h1>
-</template>
-
 <style>
-h1 {
-  color: #42b983;
-  box-shadow: 0 0 10px #42b983;
+html body {
+  display: flex;
+  justify-content: center;
+  background-image: url(https://free.picui.cn/free/2025/11/06/690c8ffcadcec.jpg);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
 }
 </style>
