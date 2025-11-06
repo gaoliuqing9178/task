7d37d65556d9d62a366e0ce0f007e161fdffebea
gaoliuqing9178 <lx8nb12act@163.com>
2025-11-06 19:25:37 +0800

    test

--- vue-task/src/App.vue
+++ vue-task/src/App.vue
@@ -1,85 +1,39 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+const name = ref('555664')
+const unumber = ref(777899)
+let data = ref(null)
+function addunumber() {
+  unumber.value++
+}
+async function getData() {
+  try {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    if (!res.ok) {
+      throw new Error(`请求失败: ${res.status}`)
+    }
+    const udata = await res.json()
+    data.value = udata.title
+    console.log(data.value)
+  } catch (error) {
+    console.error(error)
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <input type="text" v-model="name" />
+  <h1>hello world</h1>
+  <h1>{{ name }}</h1>
+  <h1>{{ unumber }}</h1>
+  <button @click="addunumber">+1</button>
+  <button @click="getData">getData</button>
+  <h1>{{ data }}</h1>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+<style>
+h1 {
+  color: #42b983;
+  box-shadow: 0 0 10px #42b983;
 }
 </style>
